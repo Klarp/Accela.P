fddb4a6fe70fc616b8dcfee2adda8d26b89b4040
Brody Jagoe <68491774+Klarp@users.noreply.github.com>
2022-06-09 21:41:55 -0600

    Update say.js

--- commands/Owner/say.js
+++ commands/Owner/say.js
@@ -11,42 +11,42 @@ module.exports = {
 	execute(message, args) {
 		if (args[0] === '-g') {
 			if (args[1]) {
-				const argMessage = args.slice(1);
+				const argMessage = args.slice(1).join(' ');
 				Client.guilds.cache.get('98226572468690944').channels.cache.get('98226572468690944').send(argMessage);
 			}
 		}
 
 		if (args[0] === '-w') {
 			if (args[1]) {
-				const argMessage = args.slice(1);
+				const argMessage = args.slice(1).join(' ');
 				Client.guilds.cache.get('98226572468690944').channels.cache.get('158484765136125952').send(argMessage);
 			}
 		}
 
 		if (args[0] === '-osu') {
 			if (args[1]) {
-				const argMessage = args.slice(1);
+				const argMessage = args.slice(1).join(' ');
 				Client.guilds.cache.get('98226572468690944').channels.cache.get('98227800330227712').send(argMessage);
 			}
 		}
 
 		if (args[0] === '-off') {
 			if (args[1]) {
-				const argMessage = args.slice(1);
+				const argMessage = args.slice(1).join(' ');
 				Client.guilds.cache.get('98226572468690944').channels.cache.get('158481913055674368').send(argMessage);
 			}
 		}
 
 		if (args[0] === '-m') {
 			if (args[1]) {
-				const argMessage = args.slice(1);
+				const argMessage = args.slice(1).join(' ');
 				Client.guilds.cache.get('98226572468690944').channels.cache.get('739447503115649054').send(argMessage);
 			}
 		}
 
 		if (args[0] === '-b') {
 			if (args[1]) {
-				const argMessage = args.slice(1);
+				const argMessage = args.slice(1).join(' ');
 				Client.guilds.cache.get('98226572468690944').channels.cache.get('137555659767021569').send(argMessage);
 			}
 		}
