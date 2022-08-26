d93acf03e1a74fa1273b662d997de00cfb09aca7
Brody Jagoe <68491774+Klarp@users.noreply.github.com>
2022-08-25 22:49:23 -0400

    Burning Text Changes
    - Increased cooldown to 1 hour
    - Disabled in non-osu channels

--- src/commands/Fun/skill.js
+++ src/commands/Fun/skill.js
@@ -5,7 +5,8 @@ module.exports = {
 	aliases: ['skillissue', 'issue'],
 	description: 'You got an issue with your skill',
 	module: 'Fun',
-	cooldown: 60,
+	cooldown: 3600,
+	disableOsu: true,
 	execute(message) {
 		message.channel.send('https://cdn.discordapp.com/attachments/98226572468690944/888634530355871794/cooltext393339445436163.gif');
 	},
--- src/commands/Fun/talkmore.js
+++ src/commands/Fun/talkmore.js
@@ -5,8 +5,8 @@ module.exports = {
 	aliases: 'talkmore',
 	description: 'How to level up on the osu! discord',
 	module: 'Fun',
-	osuDiscord: true,
-	cooldown: 60,
+	cooldown: 3600,
+	disableOsu: true,
 	execute(message) {
 		message.channel.send('https://cdn.discordapp.com/attachments/780131105725480972/792982063707848754/talkmore.gif');
 	},
