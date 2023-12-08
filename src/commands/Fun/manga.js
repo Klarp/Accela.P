b66776738172fc52559694c4429821571905410d
Brody Jagoe <68491774+Klarp@users.noreply.github.com>
2023-12-08 15:39:06 -0700

    Update manga.js
    Not even going to ask why this was here

--- src/commands/Fun/manga.js
+++ src/commands/Fun/manga.js
@@ -48,7 +48,7 @@ module.exports = {
 					.setDescription(`${englishTitle}
 		
 **Status:** ${status} | **Format:** ${format} | **Average Score:** ${avgScore}
-**Staff:** ${staffNames} filter
+**Staff:** ${staffNames}
 **Genres:** ${genres}
 
 **Volumes:** ${volumes} | **Chapters:** ${chapters}
